a480650a2d900509289befb0150ed073b16a724d
nombre del usuario 2 <usuario2@example.com>
2026-04-09 12:51:58 +0200

    Apartado C

---
+++ src/main/java/es/tienda/modelos/Articulo.java
@@ -0,0 +1,21 @@
+package es.tienda.modelos;
+
+public class Articulo {
+    private String codigo;
+    private String descripcion;
+    private double precio;
+
+    public Articulo(String codigo, String descripcion, double precio) {
+        this.codigo = codigo;
+        this.descripcion = descripcion;
+        this.precio = precio;
+    }
+
+    public Articulo Alta(String codigo, String descripcion, double precio){
+        return new Articulo(codigo, descripcion, precio);
+    }
+
+    public void VisualizarDatos(Articulo articulo){
+        System.out.printf("El articulo con código %s y descripción %s, tiene un precio de %5.2f", articulo.codigo,articulo.descripcion,articulo.precio);
+    }
+}
